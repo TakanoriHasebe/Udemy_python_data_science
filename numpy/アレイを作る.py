8612a22faa3b8e9b357d8d717ed65c82cc9fd34f
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2018-05-01 18:35:37 +0900

    アレイまで

---
+++ numpy/アレイを作る.py
@@ -0,0 +1,32 @@
+# 普通のリストの作成
+my_list1 = [1,2,3,4]
+
+# numpy形式に変換
+my_array1 = np.array(my_list1)
+
+# 多次元配列を作成する
+my_list2 = [11,22,33,44]
+my_lists = [my_list1, my_list2]
+
+# 多次元配列が作成された
+my_lists
+
+# numpy型の多次元配列の作成
+my_array2 = np.array(my_lists)
+
+# 各成分が1の5行５列の行列を作成
+np.ones((5,5))
+
+# 単位行列の作成
+np.eye(5)
+
+
+
+
+
+
+
+
+
+
+
